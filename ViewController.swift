ad78f4067f0c028169d27f2ca5c927bdb896b6f2
a-altan <57541816+a-altan@users.noreply.github.com>
2021-06-11 14:31:57 +0300

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  Alerts
+//
+//  Created by ata altan on 11.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var userNameText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    @IBOutlet weak var rePasswordText: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func signUpButton(_ sender: Any) {
+        /*
+        let alert = UIAlertController(title: "Error!", message: "UserName not found!", preferredStyle: UIAlertController.Style.alert)
+        
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default) { UIAlertAction in
+            // button clicked
+            print("button clicked ")
+            
+        }
+        alert.addAction(okButton)
+        
+        self.present(alert, animated: true, completion: nil)
+        */
+        if userNameText.text == ""{
+            makeAlert(titleInput: "Error ! ", messageInput: "UserName Not Found")
+            
+        } else if passwordText.text == ""{
+            makeAlert(titleInput: "Error ! ", messageInput: "Password Not Found")
+            
+        } else if passwordText.text != rePasswordText.text{
+            makeAlert(titleInput: "Error !", messageInput: "Password Do Not Match")
+        } else {
+           makeAlert(titleInput: "Succes", messageInput:   "User OK" )
+        }
+    }
+
+    func makeAlert(titleInput: String, messageInput:String){
+        let alert = UIAlertController(title: titleInput,message: messageInput, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+        alert.addAction(okButton)
+        self.present(alert, animated: true, completion: nil)
+    }
+
+}
+
